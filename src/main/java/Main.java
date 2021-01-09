da98769480e607cf5535375d9e790f449e39c6c4
Kewsh <kooshakazemi@gmail.com>
2021-01-09 23:57:17 +0330

    rounded corners, hover, text in shapes, extended menu bar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,16 @@
+import com.jfoenix.controls.JFXButton;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -26,51 +26,41 @@ public class Main extends Application {
         Separator separator3 = new Separator(Orientation.HORIZONTAL);
 
         Circle circle = new Circle();
-        circle.setCenterX(300);
-        circle.setCenterY(300);
         circle.setRadius(35);
-        circle.setStroke(Color.valueOf("#ff00ff"));
-        circle.setStrokeWidth(5);
-        circle.setFill(Color.BLACK);
+        circle.setFill(Color.YELLOW);
+
+        final Text text = new Text ("circ");                        // circle with text in it
+        text.setFont(new Font("monospace", 20));
+        final StackPane stack = new StackPane();
+        stack.getChildren().addAll(circle, text);
+        stack.setLayoutX(30);
+        stack.setLayoutY(30);
 
         Rectangle rectangle = new Rectangle();
-        rectangle.setX(200);
-        rectangle.setY(200);
         rectangle.setWidth(80);
         rectangle.setHeight(100);
-        rectangle.setStroke(Color.TRANSPARENT);
-        rectangle.setFill(Color.BLACK);
+        rectangle.setFill(Color.YELLOW);
 
-        HBox hBox2 = new HBox(circle, rectangle);
+        HBox hBox2 = new HBox(stack, rectangle);
         hBox2.setPadding(new Insets(75));
         hBox2.setSpacing(125);
 
-        Circle circle2 = new Circle();
-        circle.setCenterX(300);
-        circle.setCenterY(30129);
-        circle.setRadius(35);
-        circle.setStroke(Color.valueOf("#ff00ff"));
-        circle.setStrokeWidth(5);
-        circle.setFill(Color.BLACK);
-
-        Rectangle rectangle2 = new Rectangle();
-        rectangle.setX(200);
-        rectangle.setY(200);
-        rectangle.setWidth(80);
-        rectangle.setHeight(100);
-        rectangle.setStroke(Color.TRANSPARENT);
-        rectangle.setFill(Color.BLACK);
-
-        Separator separator4 = new Separator(Orientation.HORIZONTAL);
         Label label2 = new Label("hi");
         label2.setId("111");
 
-        HBox hBox3 = new HBox(circle2, rectangle2);
-        hBox3.setPadding(new Insets(75));
-        hBox3.setSpacing(125);
+        HBox hBox3 = new HBox();
+        JFXButton b1 = new JFXButton("button1");
+        b1.setId("butt1");
+        JFXButton b2 = new JFXButton("button2");
+        b2.setId("butt2");
+        hBox3.getChildren().add(b1);
+        hBox3.getChildren().add(b2);
+        hBox3.setPadding(new Insets(400, 25, 0, 25));
+        hBox3.setSpacing(25);
 
-
-        VBox leftControl  = new VBox(label, separator3, hBox2, separator4, label2);
+        VBox leftControl  = new VBox(label, separator3, hBox2, new Separator(Orientation.HORIZONTAL), label2);
+        leftControl.getChildren().add(new Separator(Orientation.HORIZONTAL));
+        leftControl.getChildren().add(hBox3);
         leftControl.setMinWidth(500);
         leftControl.setId("tools");
 
@@ -88,8 +78,10 @@ public class Main extends Application {
         HBox hBox =  new HBox(leftControl, separator, rightControl);
 
         Menu menu1 = new Menu("Menu 1");
+        Menu menu2 = new Menu("Menu 2");
         MenuBar menuBar = new MenuBar();
         menuBar.getMenus().add(menu1);
+        menuBar.getMenus().add(menu2);
 
         BorderPane borderPane = new BorderPane();
         borderPane.setTop(menuBar);
