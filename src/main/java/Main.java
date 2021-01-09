cd29fd741b6c3ef1167c54b024c66175d495a098
Kewsh <kooshakazemi@gmail.com>
2021-01-09 22:38:47 +0330

    added some stuff to the left panel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,76 @@
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
-import javafx.scene.layout.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
-import java.io.FileNotFoundException;
 
 public class Main extends Application {
 
-// splitpane -> l: whatever, r: scrollpane
     public static void main(String[] args) {
         launch(args);
     }
 
-    public void start(Stage primaryStage) throws FileNotFoundException {
+    public void start(Stage primaryStage) {
+
+        Label label = new Label("Shapes");
+        label.setId("11");
+        Separator separator3 = new Separator(Orientation.HORIZONTAL);
+
+        Circle circle = new Circle();
+        circle.setCenterX(300);
+        circle.setCenterY(300);
+        circle.setRadius(35);
+        circle.setStroke(Color.valueOf("#ff00ff"));
+        circle.setStrokeWidth(5);
+        circle.setFill(Color.BLACK);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setX(200);
+        rectangle.setY(200);
+        rectangle.setWidth(80);
+        rectangle.setHeight(100);
+        rectangle.setStroke(Color.TRANSPARENT);
+        rectangle.setFill(Color.BLACK);
+
+        HBox hBox2 = new HBox(circle, rectangle);
+        hBox2.setPadding(new Insets(75));
+        hBox2.setSpacing(125);
+
+        Circle circle2 = new Circle();
+        circle.setCenterX(300);
+        circle.setCenterY(30129);
+        circle.setRadius(35);
+        circle.setStroke(Color.valueOf("#ff00ff"));
+        circle.setStrokeWidth(5);
+        circle.setFill(Color.BLACK);
+
+        Rectangle rectangle2 = new Rectangle();
+        rectangle.setX(200);
+        rectangle.setY(200);
+        rectangle.setWidth(80);
+        rectangle.setHeight(100);
+        rectangle.setStroke(Color.TRANSPARENT);
+        rectangle.setFill(Color.BLACK);
+
+        Separator separator4 = new Separator(Orientation.HORIZONTAL);
+        Label label2 = new Label("hi");
+        label2.setId("111");
+
+        HBox hBox3 = new HBox(circle2, rectangle2);
+        hBox3.setPadding(new Insets(75));
+        hBox3.setSpacing(125);
 
 
-        VBox leftControl  = new VBox(new Label("Left Control"));
+        VBox leftControl  = new VBox(label, separator3, hBox2, separator4, label2);
         leftControl.setMinWidth(500);
         leftControl.setId("tools");
 
@@ -54,7 +103,7 @@ public class Main extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("JavaFX App");
-        primaryStage.setFullScreen(true);
+        primaryStage.setMaximized(true);
         primaryStage.show();
 
     }
